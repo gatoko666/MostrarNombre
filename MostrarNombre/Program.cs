0464d2763000b6be142df802787d6eb679eb1664
gatoko <g.millaquiensuarez@gmail.com>
2018-02-28 22:46:26 -0300

    dsfsdfsdf

---
+++ MostrarNombre/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HolaMundoC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PreguntaNombreyEdad();
+        }
+
+
+
+        static void PreguntaNombreyEdad()
+        {
+            string nombre;
+            int edad;
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Ingrese su edad: ");
+            edad = int.Parse(Console.ReadLine());
+
+            string output = string.Format("Su nombre es {0} y tiene {1} años de edad.", nombre, edad);
+            Console.WriteLine(output);
+            Console.Read();
+        }
+    }
+}
